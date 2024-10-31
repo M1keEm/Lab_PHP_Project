e1cd2971cc306a212e5333cbba3f95e7c4041142
MikeEm <michalbanaszek03@gmail.com>
2024-10-31 01:19:28 +0100

    klient.php

---
+++ klient.php
@@ -0,0 +1,6 @@
+<h4>Informacje kontaktowe </h4>
+<p> Nazwisko: <?php echo $_GET['nazwisko']; ?> <br/>
+    Wiek: <?php echo $_GET['wiek']; ?> <br/>
+    Państwo: <?php echo $_GET['panstwo']; ?><br/>
+    Adres email: <?php echo $_GET['email']; ?>
+</p>
