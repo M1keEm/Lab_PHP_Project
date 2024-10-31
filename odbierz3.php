4e19a01aee1e6c9a118b997e70a6a78f50e2ad7f
MikeEm <michalbanaszek03@gmail.com>
2024-10-31 01:19:40 +0100

    odbierz3.php

---
+++ odbierz3.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Wynik zamówienia</title>
+</head>
+<body>
+<h2>Wybrane języki:</h2>
+<?php
+if (isset($_REQUEST['jezyki'])) {
+    echo "Wybrane tutoriale foreach:";
+    foreach($_REQUEST['jezyki'] as $jezyk) {
+        echo " ".$jezyk," ";
+    }
+    echo "<br>wybrane tutoriale join: ". join(", ", $_REQUEST['jezyki']);
+} else {
+    echo "<p>Nie wybrano żadnych tutoriali.</p>";
+}
+
+echo "<h2>Wszystkie parametry:</h2>";
+foreach ($_REQUEST as $key => $value) {
+    if (is_array($value)) {
+        echo "$key: " . join(", ", $value) . "<br>";
+    } else {
+        echo "$key: $value<br>";
+    }
+}
+?>
+</body>
+</html>
