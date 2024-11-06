bdec2d5380c7e8b7d76d5c0a653cd1de9bb1020d
Michał Banaszek <michalbanaszek03@gmail.com>
2024-11-06 22:22:08 +0100

    funkcje.php isset($_REQUEST["submit"]) fixed

--- Lab_3/funkcje.php
+++ Lab_3/funkcje.php
@@ -5,33 +5,39 @@ function dodaj()
     if (isset($_REQUEST["nazw"])) {
         $dane .= htmlspecialchars($_REQUEST['nazw']) . " ";
     }
+}
 
-    function pokaz()
-    {
+function pokaz()
+{
 //odczytaj wszystkie dane z pliku i wyświetl wierszami...
-    }
+}
 
-    function pokaz_zamowienie($tut)
-    {
+function pokaz_zamowienie($tut)
+{
 //odczytaj dane z pliku i wyświetl tylko te wiersze,
 //w których zamówiono tutorial $tut (np. $tut="Java")
-    }
+}
 
-    //Skrypt właściwy do obsługi akcji (żądań):
-    if (isset($_REQUEST["submit"])) { //jeśli kliknięto przycisk o name=submit
-        $akcja = $_REQUEST["submit"]; //odczytaj jego value
-        switch ($akcja) {
-            case "Zapisz":
-                dodaj();
-                break;
-            case "Pokaż":
-                pokaz();
-                break;
-            case "Java":
-                pokaz_zamowienie("Java");
-                break;
-            //pozostałe przypadki
-        }
+//Skrypt właściwy do obsługi akcji (żądań):
+if (isset($_REQUEST["submit"])) { //jeśli kliknięto przycisk o name=submit
+    $akcja = $_REQUEST["submit"]; //odczytaj jego value
+    switch ($akcja) {
+        case "Zapisz":
+            dodaj();
+            break;
+        case "Pokaż":
+            pokaz();
+            break;
+        case "Java":
+            pokaz_zamowienie("Java");
+            break;
+        case "CPP":
+            pokaz_zamowienie("CPP");
+            break;
+        case "PHP":
+            pokaz_zamowienie("PHP");
+            break;
+        //pozostałe przypadki
     }
 }
 
