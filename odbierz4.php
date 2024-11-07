a450c4df5e2a535cc8b6abafefa94a19b7b65606
MikeEm <michalbanaszek03@gmail.com>
2024-11-07 09:02:04 +0100

    odbierz4.php adjustment

--- odbierz4.php
+++ odbierz4.php
@@ -7,17 +7,17 @@
 <body>
 <h2>Wybrane języki:</h2>
 <?php
-if (isset($_REQUEST['jezyki'])) {
+if (isset($_POST['jezyki'])) {
     echo "Wybrane tutoriale:";
-    foreach ($_REQUEST['jezyki'] as $jezyk) {
+    foreach ($_POST['jezyki'] as $jezyk) {
         echo " " . $jezyk, " ";
     }
 } else {
     echo "<p>Nie wybrano żadnych tutoriali.</p>";
 }
 echo "<br>Wybrany sposób płatności: ";
-if (isset($_REQUEST['payment'])) {
-    $payment = $_REQUEST['payment'];
+if (isset($_POST['payment'])) {
+    $payment = $_POST['payment'];
     echo $payment . "<br>";
 }
 $surname = $_POST['surname'];
@@ -25,10 +25,10 @@ $age = $_POST['age'];
 $email = $_POST['email'];
 $country = $_POST['country'];
 if ($surname && $age && $email && $country) {
-    print("<p><a href = 'http://localhost:63342/Lab_PHP_Project/klient.php?nazwisko=$surname&wiek=$age&email=$email&panstwo=$country'>");
+    print("<p><a href = 'klient.php?nazwisko=$surname&wiek=$age&email=$email&panstwo=$country'>");
     print("informaje konktaktowe</a>");
 } else echo "dane zamawiającego nie są kompletne";
-echo "<p><a href = 'http://localhost:63342/Lab_PHP_Project/pliki.php'>" . "Powrót do formularza" . "</a>"
+echo "<p><a href = '/formularz.php'>" . "Powrót do formularza" . "</a>"
 ?>
 
 </body>
